a1b199bb1c45cd3e40b89b1b2ead541bdf4488a9
jamshidsalayev1998 <jmlaravel@mail.ru>
2024-05-06 22:07:47 +0500

    dasd

--- app/Handlers/CustomTelegramBotHandler.php
+++ app/Handlers/CustomTelegramBotHandler.php
@@ -3,9 +3,18 @@
 namespace App\Handlers;
 
 use DefStudio\Telegraph\Handlers\WebhookHandler;
+use Illuminate\Support\Stringable;
 
 class CustomTelegramBotHandler extends WebhookHandler
 {
+    protected function handleChatMessage(Stringable $text)
+    {
+        $this->chat->html("message : $text")->send();
+    }
+    protected function handleUnknownCommand(Stringable $text)
+    {
+        $this->chat->html("I can't understand your command: $text")->send();
+    }
     public function start()
     {
         $bot = $this->chat->bot;
