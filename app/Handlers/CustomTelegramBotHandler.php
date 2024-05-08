b381552efd5ace740336dcadba59e95bb066eae4
Jamshid <57948636+jamshidsalayev1998@users.noreply.github.com>
2024-05-08 14:42:32 +0500

    Update CustomTelegramBotHandler.php

--- app/Handlers/CustomTelegramBotHandler.php
+++ app/Handlers/CustomTelegramBotHandler.php
@@ -2,6 +2,7 @@
 
 namespace App\Handlers;
 
+use App\Models\TempMessage;
 use DefStudio\Telegraph\Handlers\WebhookHandler;
 use Illuminate\Support\Stringable;
 
@@ -9,14 +10,17 @@ class CustomTelegramBotHandler extends WebhookHandler
 {
     protected function handleChatMessage(Stringable $text): void
     {
+        TempMessage::create(['text_response' => 'chat message ga galdi']);
         $this->chat->html("message : $text")->send();
     }
     protected function handleUnknownCommand(Stringable $text): void
     {
+        TempMessage::create(['text_response' => 'unknown a galdi']);
         $this->chat->html("I can't understand your command: $text")->send();
     }
     public function start()
     {
+        TempMessage::create(['text_response' => 'starta galdi']);
         $bot = $this->chat->bot;
         $text = 'Bot ishlashni boshladi';
         $this->chat->html($text)->send();
