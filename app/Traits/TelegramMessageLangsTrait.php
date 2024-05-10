1800268a1dd21b192add48af9bcb3aa55dc74c3d
jamshidsalayev1998 <jmlaravel@mail.ru>
2024-05-10 23:09:32 +0500

    Update TelegramMessageLangsTrait.php

--- app/Traits/TelegramMessageLangsTrait.php
+++ app/Traits/TelegramMessageLangsTrait.php
@@ -1,13 +1,16 @@
 <?php
+
 namespace App\Traits;
-trait TelegramMessageLangsTrait{
-    public static function lang($messageKeyWord){
+
+trait TelegramMessageLangsTrait
+{
+    public static function lang($messageKeyWord)
+    {
         $messages = config('message_langs');
-        $locale = app()->getLocale();
-        if(key_exists($messageKeyWord , $messages[$locale])){
+        $locale = app()->getLocale() ? app()->getLocale() : 'uz';
+        if (key_exists($messageKeyWord, $messages[$locale])) {
             return $messages[$locale][$messageKeyWord];
-        }
-        else{
+        } else {
             return $messageKeyWord;
         }
     }
