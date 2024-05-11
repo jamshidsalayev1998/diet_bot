7f093d18083f5ec1fb7fa6bb6ae6b3f3e8e64f61
jamshidsalayev1998 <jmlaravel@mail.ru>
2024-05-11 12:24:01 +0500

    Update LangServiceProvider.php

--- app/Providers/LangServiceProvider.php
+++ app/Providers/LangServiceProvider.php
@@ -28,10 +28,13 @@ class LangServiceProvider extends ServiceProvider
         ]);
         $data = $request->all();
         $userInfo = null;
-        if (key_exists('message', $data))
+        if (key_exists('message', $data)) {
             $userInfo = UserInfo::where('chat_id', $data['message']['from']['id'])->orderBy('id', 'ASC')->first();
+        } elseif (key_exists('callback_query', $data)) {
+            $userInfo = UserInfo::where('chat_id', $data['callback_query']['from']['id'])->orderBy('id', 'ASC')->first();
+        }
         if (!$userInfo) {
-            $locale = config('app.locale' , 'uz');
+            $locale = config('app.locale', 'uz');
         } else {
             $locale = $userInfo->language;
         }
