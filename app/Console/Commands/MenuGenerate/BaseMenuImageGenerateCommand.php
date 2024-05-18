642beca905af09e8dab7338d1055adda92624f32
jamshidsalayev1998 <jmlaravel@mail.ru>
2024-05-18 20:21:44 +0500

    dasd

---
+++ app/Console/Commands/MenuGenerate/BaseMenuImageGenerateCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands\MenuGenerate;
+
+use App\Services\FileSave;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Drivers\Imagick\Driver;
+use Intervention\Image\ImageManager;
+
+// use Intervention\Image\Facades\Image;
+
+class BaseMenuImageGenerateCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'menuGenerate:baseMenuGenerate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $manager = new ImageManager(Driver::class);
+
+        // create new image 640x480
+        $image = $manager->create(640, 480);
+
+        // create new image 512x512 with grey background
+        $image = $manager->create(512, 512)->fill('ccc');
+        $fileUrl = FileSave::storeFile('/menu/base/'.date('Y-m-d') , $image);
+        $this->info($fileUrl);
+    }
+}
---
+++ app/Services/FileSave.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services;
+
+class FileSave
+{
+    public static function storeFile($folder, $file)
+    {
+        $nameFile = date('Y-m-d').'_'.date('H-i-s').'_'.RandomStringService::randomAlphaAndNumberHelper(10);
+        $extension = $file->getClientOriginalExtension();
+        $nameFile.='.'.$extension;
+        $file->move('files/'.$folder,$nameFile);
+        return '/files/'.$folder.'/'.$nameFile;
+    }
+}
