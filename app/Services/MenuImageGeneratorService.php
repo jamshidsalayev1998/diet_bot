5c48f45c94e8b660d1be5cd50195ed54df57002e
jamshidsalayev1998 <jmlaravel@mail.ru>
2024-06-06 20:32:31 +0500

    Update MenuImageGeneratorService.php

--- app/Services/MenuImageGeneratorService.php
+++ app/Services/MenuImageGeneratorService.php
@@ -38,7 +38,7 @@ class MenuImageGeneratorService
                     }
                 }
                 // $this->info(json_encode($ready[1]['records'][0]));
-                $imageUrl = '/menu_images/' . date('Y-m-d') . '/' . $menuSize->calories . '/menu/' . $userInfo->id . '/' . $userInfo->language . '.jpg';
+                $imageUrl = '/menu_images/' . date('Y-m-d') . '/' . $menuSize->calories . '/menu/' . RandomStringService::randomAlphaAndNumberHelper(15) . '/' . $userInfo->language . '.jpg';
                 $url = 'app/public' . $imageUrl;
                 $imagePath = storage_path($url);
                 $directoryPath = dirname($imagePath);
