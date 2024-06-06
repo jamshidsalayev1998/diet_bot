d53d64a4c78c508e6a99760bd128caa724fe5f78
jamshidsalayev1998 <jmlaravel@mail.ru>
2024-06-06 19:54:54 +0500

    Update MenuImageGeneratorService.php

--- app/Services/MenuImageGeneratorService.php
+++ app/Services/MenuImageGeneratorService.php
@@ -50,7 +50,7 @@ class MenuImageGeneratorService
                 $htmlContent = view('menu_images.base_menu_template', ['data' => $ready, 'lang' => $userInfo->language, 'user_info' => $userInfo, 'menu_rules' => $menuRules])->render();
                 $htmlFilePath = storage_path('app/public/image_html/base_menu.html');
                 file_put_contents($htmlFilePath, $htmlContent);
-                $command = "wkhtmltoimage --width 1000  --quality 50 {$htmlFilePath} {$imagePath}";
+                $command = "wkhtmltoimage  {$htmlFilePath} {$imagePath}";
                 shell_exec($command);
                 $userInfo->menu_image = $imageUrl;
                 $userInfo->update();
