e6aa03b6435c08a9cd028bd12d90b77a7bb7ddee
jamshidsalayev1998 <jmlaravel@mail.ru>
2024-07-02 19:46:56 +0500

    Update TurnOffTheOldConversationsCommand.php

--- app/Console/Commands/CalcAi/TurnOffTheOldConversationsCommand.php
+++ app/Console/Commands/CalcAi/TurnOffTheOldConversationsCommand.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands\CalcAi;
 use Illuminate\Console\Command;
 use Carbon\Carbon;
 use App\Models\V1\CalcAiConversation;
+use App\Services\CalcAi\CalcAiService;
 use App\Services\TelegramButtonService;
 
 class TurnOffTheOldConversationsCommand extends Command
@@ -32,6 +33,9 @@ class TurnOffTheOldConversationsCommand extends Command
         $conversations = CalcAiConversation::where('created_at', '<', $fiveMinuteAgo)->get();
         foreach($conversations as $conversation){
             $telegraph_chat = $conversation->telegraph_chat;
+            if($conversation->product_id && $conversation->status == 1){
+                CalcAiService::delete_product_ai($conversation->product_id , '6502974186:AAHY3T5E9jkXNre7aqZ0ShvNt25x23mC0DUzafar0000A');
+            }
             TelegramButtonService::stop_calc_ai_conversation($telegraph_chat);
             $conversation->delete();
         }
